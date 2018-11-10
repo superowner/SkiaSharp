e96406a0c5da2b36bebc232346f6a9c2f178e333
Matthew Leibowitz <mattleibow@live.com>
2018-11-10 15:30:00 +0200

    dev/update-native

--- binding/Binding/SKPath.cs
+++ binding/Binding/SKPath.cs
@@ -119,11 +119,8 @@ namespace SkiaSharp
 
 		public SKRect Bounds {
 			get {
-				if (GetBounds (out var rect)) {
-					return rect;
-				} else {
-					return SKRect.Empty;
-				}
+				GetBounds (out var rect);
+				return rect;
 			}
 		}
 
@@ -404,7 +401,13 @@ namespace SkiaSharp
 
 		public bool GetBounds (out SKRect rect)
 		{
-			return SkiaApi.sk_path_get_bounds (Handle, out rect);
+			var empty = IsEmpty;
+			if (empty) {
+				rect = SKRect.Empty;
+			} else {
+				SkiaApi.sk_path_get_bounds (Handle, out rect);
+			}
+			return empty;
 		}
 
 		public SKRect ComputeTightBounds ()
